7383fc09718cfff6d44d55b96a849364910d31da
Sergey S <volshibenik@gmail.com>
2017-09-28 11:31:16 +0300

    got rid of body array, added head and tail and append method, marked tail/head props with _ to differ from same methods

--- src/linked-list.js
+++ src/linked-list.js
@@ -2,36 +2,41 @@ const Node = require('./node');
 
 class LinkedList {
     constructor() {
-      this.head = null;
-      this.tail = null;
+      this._head = null;
+      this._tail = null;
       this.length = 0;
   
     }
-    append(data) {
-      var newNode = new Node(data);
-      var targetNode = this.head;
-  
-  //    console.log('newNex',newNode.next);
-  
-      if (!targetNode) {
-        this.head = newNode;
-        this.length++;
-      } else {
-  
-        for (var i = 1; i < this.length; i++) {
-          targetNode = targetNode.next;
-        }
-        targetNode.next = newNode;
-        this.length++;
-  
-      }
-  
-    }
+		append(data) {
+			var newNode = new Node(data);
+			var targetNode = this._head;
+	
+	
+			if (!targetNode) {
+				this._head = newNode;
+				this._tail = newNode;
+				this.length++;
+			} else {
+	
+				for (var i = 1; i < this.length; i++) {  
+					targetNode = targetNode.next;   //flipping through the end
+				}
+				this._tail = newNode;
+				targetNode.next = this._tail;
+				targetNode.next.prev = targetNode;
+	
+				this.length++;
+	
+			}
+	
+		}
 
     head() {
+			return this._head['data'];
     }
 
     tail() {
+			return this._tail['data'];
     }
 
     at(index) {
