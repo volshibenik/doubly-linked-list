a833f2bfd4267b384c08faf1aa6dff3f2f57b92c
Sergey S <volshibenik@gmail.com>
2017-09-28 17:44:21 +0300

    struggling with reverse

--- src/linked-list.js
+++ src/linked-list.js
@@ -33,11 +33,11 @@ class LinkedList {
 		}
 
     head() {
-			return this._head['data'];
+			return this._head?  this._head['data'] : this._head;
     }
 
     tail() {
-			return this._tail['data'];
+			return this._tail?  this._tail['data'] : this._tail;
     }
 
     at(index) {
@@ -49,15 +49,38 @@ class LinkedList {
     }
 
     insertAt(index, data) {
+			var targetNode = this._head;
+			
+			for (var i = 0; i < index; i++) {  
+				targetNode = targetNode.next;   //flipping through the end
+			}
+			var newNode = new Node(data, targetNode.prev, targetNode);
+			targetNode.prev.next = newNode;
+			targetNode.prev = newNode;
+			this.length++
     }
 
     isEmpty() {
+			return this._head === null
     }
 
     clear() {
+			this._head = null;
+			this._tail = null;
+			this.length = 0;
     }
 
     deleteAt(index) {
+			var targetNode = this._head;
+
+			for (var i = 0; i < index; i++) {  
+				targetNode = targetNode.next;   //flipping through the end
+			}
+			
+			targetNode.prev.next = targetNode.next;
+			targetNode.next.prev = targetNode.prev;
+			
+			this.length--
     }
 
     reverse() {
